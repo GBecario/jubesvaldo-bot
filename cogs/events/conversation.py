0d67b35b076e3236983599ae9eea99887daa0c31
GBecario <gustavobecario1@gmail.com>
2025-11-05 17:59:28 -0300

    refactor: movendo um ID para um arquivo .env e implementando leitura

--- cogs/events/conversation.py
+++ cogs/events/conversation.py
@@ -1,6 +1,11 @@
 import discord
 from discord.ext import commands
 from discord import app_commands
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+CATEGORY_ID = int(os.getenv("CATEGORY_ID"))
 
 class conversation(commands.Cog):
     def __init__(self, client:commands.Bot):
@@ -8,11 +13,11 @@ class conversation(commands.Cog):
     
     @commands.Cog.listener()
     async def on_message(self, message:discord.Message):
-        if message.content.startswith("Olá jubesvaldo") and message.content.endswith("tudo bem?") and message.channel.category_id == 1364692042147561482:
+        if message.content.startswith("Olá jubesvaldo") and message.content.endswith("tudo bem?") and message.channel.category_id == CATEGORY_ID:
             print(f"Autor: {message.author}")
             print(f"message: {message.content}")
             await message.reply(f"Olá {message.author.mention}, por aqui está tudo bem!")
-        elif message.content.startswith("Olá jubesvaldo") and message.channel.category_id == 1364692042147561482:
+        elif message.content.startswith("Olá jubesvaldo") and message.channel.category_id == CATEGORY_ID:
             print(f"Autor: {message.author}")
             print(f"message: {message.content}")
             await message.reply(f"Olá {message.author.mention}, tudo bem?")
